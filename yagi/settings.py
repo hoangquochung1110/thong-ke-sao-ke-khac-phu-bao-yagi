c927cd4fd6d2e80f83b54e9b3472d2e15ce21563
hoangquochung1110 <arsenalforeversince2007@gmail.com>
2024-09-14 01:55:07 +0700

    Set DB_HOST

--- yagi/settings.py
+++ yagi/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'NAME': env.str('DB_NAME'),
         'USER': env.str('DB_USER'),
         'PASSWORD': env.str('DB_PASSWORD'),
-        'HOST': 'localhost',
+        'HOST': env.str('DB_HOST'),
         'PORT': '5432',
     }
 }
